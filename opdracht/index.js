024b273b2c036505a1327b8625acf18b1cfd0eb3
Fishezzz RPi <33294032+Fishezzz@users.noreply.github.com>
2019-11-15 02:33:26 +0100

    hard links

--- opdracht/index.js
+++ opdracht/index.js
@@ -1,1 +1,42 @@
-/var/www/html/opdracht/index.js
+fetch('./data.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        // Work with JSON data here
+        console.log(result);
+        fillTable(result);
+    })
+    .catch(err => {
+        console.log('Error: ' + err);
+    })
+
+function fillTable(data) {
+    data.forEach(entry => {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.innerHTML = entry.name;
+        tr.appendChild(td);
+        var td = document.createElement('td');
+        td.innerHTML = entry.age;
+        tr.appendChild(td);
+        var td = document.createElement('td');
+        td.innerHTML = getTimeString(entry.timestamp);
+        tr.appendChild(td);
+        document.getElementById("dataTable").appendChild(tr);
+    });
+}
+
+function getTimeString(time) {
+    var date = new Date(time * 1000);
+    var year = date.getFullYear();
+    var month = "0" + date.getMonth();
+    var day = "0" + date.getDay();
+    var hours = "0" + date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+
+    var formattedTime = `${year}-${month.substr(-2)}-${day.substr(-2)} T ${hours.substr(-2)}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+
+    return formattedTime;
+}
